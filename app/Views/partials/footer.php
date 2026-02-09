bebbc3863689ec678c61cfb55e43a622b28c720d
Daniela <daniela.cali.1981@gmail.com>
2026-02-09 19:11:39 +0100

    Correzione

--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -1,9 +1,8 @@
 <footer class="bg-dark text-light py-3 text-center mt-5">
     <div>
-        <small>&copy; <?= date('Y') ?> MeTe Licenze - Tutti i diritti riservati <em>Per info: metelicenze dot gmail.com</em></small>
+        <small>&copy; <?= date('Y') ?> MeTe Licenze - Tutti i diritti riservati</small>
     </div>
     <div>
         <small><em>Per informazioni: metelicenze at gmail dot com</em></small>
     </div>
-
 </footer>
